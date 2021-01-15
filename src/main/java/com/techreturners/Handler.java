94d8fd817127386064b371ef1622800f8c6e2d63
James Heggs <james.heggs@techreturners.com>
2021-01-15 18:15:13 +0000

    Modelled in task object

--- src/main/java/com/techreturners/Handler.java
+++ src/main/java/com/techreturners/Handler.java
@@ -1,8 +1,11 @@
-package com.serverless;
+package com.techreturners;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
+import com.techreturners.model.Task;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -15,12 +18,18 @@ public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayRe
 
 	@Override
 	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
-		LOG.info("received: {}", input);
-		Response responseBody = new Response("Hello Tech Returners", input);
+		LOG.info("received the request");
+
+
+		Task t1 = new Task("abc1234", "Pick up the newspapers", false);
+		Task t2 = new Task("abc4567", "Enjoy Java!", false);
+		List<Task> tasks = new ArrayList<>();
+		tasks.add(t1);
+		tasks.add(t2);
+
 		return ApiGatewayResponse.builder()
 				.setStatusCode(200)
-				.setObjectBody(responseBody)
-				.setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & serverless"))
+				.setObjectBody(tasks)
 				.build();
 	}
 }

c5c79515316070c4ae419cccd2cf40d58292fc05
James Heggs <james.heggs@techreturners.com>
2021-01-18 08:45:00 +0000

    Refactor GET tasks handler

--- src/main/java/com/techreturners/GetTasksHandler.java
+++ src/main/java/com/techreturners/GetTasksHandler.java
@@ -1,9 +1,7 @@
 package com.techreturners;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
@@ -16,9 +14,9 @@ import org.apache.logging.log4j.Logger;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-public class Handler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+public class GetTasksHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
-	private static final Logger LOG = LogManager.getLogger(Handler.class);
+	private static final Logger LOG = LogManager.getLogger(GetTasksHandler.class);
 
 	@Override
 	public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent request, Context context) {
